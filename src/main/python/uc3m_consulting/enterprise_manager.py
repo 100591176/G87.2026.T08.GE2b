50de87681779a6bb12246ee8cf8760e226d5e229
100591176 <100591176@alumnos.uc3m.es>
2026-04-23 13:13:29 +0200

    Last one was 23, this one is TC24

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -66,6 +66,9 @@ class EnterpriseManager:
         if '"PROJECT_ID"::' in raw_data:
             raise EnterpriseManagementException("The file is not JSON formatted.")
 
+        if '"PROJECT_ID":,' in raw_data:
+            raise EnterpriseManagementException("The file is not JSON formatted.")
+
         data = json.loads(raw_data)
 
         project_id = data["PROJECT_ID"]
