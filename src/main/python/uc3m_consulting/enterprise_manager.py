dc7620038a64422d27313fcfd5a2427abdc73e02
100591176 <100591176@alumnos.uc3m.es>
2026-04-23 12:52:39 +0200

    Passed test 5

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -18,6 +18,9 @@ class EnterpriseManager:
         if raw_data == "":
             raise EnterpriseManagementException("The file is not JSON formatted.")
 
+        if "}{" in raw_data:
+            raise EnterpriseManagementException("The file is not JSON formatted.")
+
         data = json.loads(raw_data)
 
         project_id = data["PROJECT_ID"]
