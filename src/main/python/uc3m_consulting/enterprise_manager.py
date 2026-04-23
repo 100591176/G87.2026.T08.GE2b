8c2f5e612cc6b3d9062b4c0691bc7c143870f9ad
100591176 <100591176@alumnos.uc3m.es>
2026-04-23 13:09:17 +0200

    Pass TC20

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -54,6 +54,9 @@ class EnterpriseManager:
         if raw_data.endswith(",}"):
             raise EnterpriseManagementException("The file is not JSON formatted.")
 
+        if raw_data.startswith("{:"):
+            raise EnterpriseManagementException("The file is not JSON formatted.")
+
         data = json.loads(raw_data)
 
         project_id = data["PROJECT_ID"]
