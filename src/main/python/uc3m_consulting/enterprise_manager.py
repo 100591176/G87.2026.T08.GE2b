b101cd40e5849bb16278fecb5cc616bc18180609
100591176 <100591176@alumnos.uc3m.es>
2026-04-23 12:54:43 +0200

    Passed T08

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -24,6 +24,9 @@ class EnterpriseManager:
         if raw_data.startswith('"PROJECT_ID"'):
             raise EnterpriseManagementException("The file is not JSON formatted.")
 
+        if raw_data.startswith("{{"):
+            raise EnterpriseManagementException("The file is not JSON formatted.")
+
         data = json.loads(raw_data)
 
         project_id = data["PROJECT_ID"]
