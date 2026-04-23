adeca6d0f4fbb610e0697d20c2da8469d12dbb3c
100591176 <100591176@alumnos.uc3m.es>
2026-04-23 12:33:26 +0200

    Updated project_document to work for code

--- src/main/python/uc3m_consulting/project_document.py
+++ src/main/python/uc3m_consulting/project_document.py
@@ -1,36 +1,42 @@
-"""Contains the class OrderShipping"""
+"""Project document entity."""
 from datetime import datetime, timezone
 import hashlib
 
-class ProjectDocument():
-    """Class representing the information required for shipping of an order"""
 
-    def __init__(self, project_id: str, file_name):
+class ProjectDocument:
+    """Class representing a registered project document."""
+
+    def __init__(self, project_id: str, file_name: str):
         self.__alg = "SHA-256"
-        self.__type = "DEPOSIT"
+        self.__typ = "DOCUMENT"
         self.__project_id = project_id
         self.__file_name = file_name
         justnow = datetime.now(timezone.utc)
         self.__register_date = datetime.timestamp(justnow)
 
-    def to_json(self):
-        """returns the object data in json format"""
-        return {"alg": self.__alg,
-                "type": self.__type,
-                "project_id": self.__project_id,
-                "file_name": self.__file_name,
-                "register_date": self.__register_date,
-                "document_signature": self.document_signature}
-
     def __signature_string(self):
-        """Composes the string to be used for generating the key for the date"""
-        return "{alg:" + str(self.__alg) +",typ:" + str(self.__type) +",project_id:" + \
-               str(self.__project_id) + ",file_name:" + str(self.__file_name) + \
-               ",register_date:" + str(self.__register_date) + "}"
+        """Build the string used to generate the SHA-256 signature."""
+        return (
+            "{alg:" + self.__alg +
+            ", typ:" + self.__typ +
+            ", project_id:" + self.__project_id +
+            ", file_name:" + self.__file_name + "}"
+        )
+
+    def to_json(self):
+        """Return the object data in JSON format."""
+        return {
+            "alg": self.__alg,
+            "typ": self.__typ,
+            "project_id": self.__project_id,
+            "file_name": self.__file_name,
+            "register_date": self.__register_date,
+            "file_signature": self.file_signature
+        }
 
     @property
     def project_id(self):
-        """Property that represents the product_id of the patient"""
+        """Return the project id."""
         return self.__project_id
 
     @project_id.setter
@@ -39,22 +45,23 @@ class ProjectDocument():
 
     @property
     def file_name(self):
-        """Property that represents the order_id"""
+        """Return the file name."""
         return self.__file_name
+
     @file_name.setter
     def file_name(self, value):
         self.__file_name = value
 
     @property
     def register_date(self):
-        """Property that represents the phone number of the client"""
+        """Return the register date."""
         return self.__register_date
+
     @register_date.setter
     def register_date(self, value):
         self.__register_date = value
 
-
     @property
-    def document_signature(self):
-        """Returns the sha256 signature of the date"""
+    def file_signature(self):
+        """Return the SHA-256 signature."""
         return hashlib.sha256(self.__signature_string().encode()).hexdigest()
